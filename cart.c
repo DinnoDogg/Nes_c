26f616befd7a2aab1984a532afc4652825fe4e4c
Mack Clog <onoah229@gmail.com>
2026-04-08 07:16:48 -0600

    updated

---
+++ cart.c
@@ -0,0 +1,7 @@
+#include <stdint.h>
+
+#include "include/cart.h"
+
+struct cart {
+
+};
---
+++ include/cart.h
@@ -0,0 +1,8 @@
+#ifndef CART_H
+#define CART_H
+
+#include <stdint.h>
+
+typedef struct cart cart_t;
+
+#endif
